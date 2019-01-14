b7af52dc827e314ecea7e9d7201495bcd489d45c
CastelH <Hernan.Castellani@unisys.com>
2019-01-14 16:15:56 -0300

    Prueba desde NB

--- wssfb.asmx.cs
+++ wssfb.asmx.cs
@@ -14,7 +14,7 @@ namespace wssfb_Ns
 {
 	/// <summary>
 	/// Summary description for wssfb.
-    /// Prueba Git3
+    /// Prueba Git desde NB
 	/// </summary>
 	[WebService(Namespace="http://www.wssfb.com/",
 		Description="sfb system wssfb web services.")]
